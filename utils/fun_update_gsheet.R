269cdadd13710d1945d1c95887116de2a61a18c3
Robert Koch <r.tobiaskoch@gmail.com>
2025-06-13 16:47:25 -0600

    fixed natural_join F-ing up the factors

--- utils/fun_update_gsheet.R
+++ utils/fun_update_gsheet.R
@@ -6,7 +6,9 @@ update_gsheet = function(new, #new data to add
                          gkey,  #google sheet key of file you want to update
                          gfolder, #folder name in google drive save copy
                          gname, #name of file to cp before updating
-                         sheet = "data"
+                         sheet = "data",
+                         col_database = c( "csu_id", "trap_id", "zone", "year", "week", "trap_date","method", "spp",
+                                           "total", "test_code", "cq", "copies_WNV", "seq", "lat", "long")
                          ) {
 
 #if old isn't supplied load from google
@@ -14,11 +16,21 @@ if(is.null(old)) {
   old = googlesheets4::read_sheet(ss = gkey, 
                             sheet = sheet)  
 }
-
   
 #update data 
-update = natural_join(new, old, jointype = type, 
-                            by = by)
+update = rquery::natural_join(
+  new, old, 
+  jointype = type, 
+  by = by) %>%
+#  mutate(across(everything(), ~ ifelse(is.na(.), "", .))) %>% #prevents NA from making non character variables in database throw errors
+  select(all_of(col_database)) #reorder them because natural_join fucked up the order
+
+#if trap_date is present convert it back to date after mutate everything changed it to a number
+if("trap_date" %in% names(update)) {
+  update = update %>%
+  mutate(trap_date = as_date(as_datetime(trap_date))) %>%
+  arrange(desc(trap_date))
+}
 
 #save local copy
 write.csv(update, fn_save)
@@ -30,8 +42,10 @@ write.csv(update, fn_save)
            overwrite = T)
   
   #save it to gdrive
+  options(googlesheets4.na = "")  # Write NA as blank cells
   googlesheets4::sheet_write(update,
-                             ss = gkey
+                             ss = gkey,
+                             sheet = sheet
   )
 }
 
