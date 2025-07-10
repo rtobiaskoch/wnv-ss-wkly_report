6976ea15fe4b6c3d6e6df4cce173c1fa316365ff
Robert Koch <r.tobiaskoch@gmail.com>
2025-07-10 15:41:57 -0600

    add install for pooled infection rate

--- config/load_packages.R
+++ config/load_packages.R
@@ -4,10 +4,15 @@
 suppressMessages({
   if (!require("pacman")) install.packages("pacman")
   pacman::p_unload()
-  pacman::p_load(argparse, #config
-                 googlesheets4, googledrive, rio, readxl, openxlsx, googledrive, #importing and exporting
-                 tidyverse, janitor, lubridate, rquery, stringr, #manipulation
-                 PooledInfRate, #analysis
-                 ggpubr, wesanderson, paletteer, leaflet, patchwork# plotting
-                )
+  pacman::p_load(
+    argparse, # config
+    googlesheets4, googledrive, rio, readxl, openxlsx, googledrive, # importing and exporting
+    tidyverse, janitor, lubridate, rquery, stringr, # manipulation
+    devtools, # analysis
+    ggpubr, wesanderson, paletteer, leaflet, patchwork # plotting
+  )
+  Sys.setenv(R_QPDF="true") # to build the vignette during the package build
+  library(devtools) # need to install this if you do not have it
+  devtools::install_github("https://github.com/CDCgov/PooledInfRate",build_vignettes = TRUE)
+  
 })
