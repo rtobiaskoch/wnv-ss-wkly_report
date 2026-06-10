6a9eabbf99354fe6159f9eb89c49c8cca96a2e42
Robert Koch <r.tobiaskoch@gmail.com>
2026-06-10 13:31:21 -0600

    weekly commit after report generation for the week

--- utils/fun_trap_stat_summ.R
+++ utils/fun_trap_stat_summ.R
@@ -1,26 +1,26 @@
-trap_stat_sum = function(df) {
-  
+trap_stat_sum = function(df, year_start = NULL, year_end = NULL) {
+  if (!is.null(year_start) & !is.null(year_end)) {
+    df = df %>%
+      dplyr::filter(year >= year_start & year <= year_end)
+  }
+
   df = df %>%
-  dplyr::filter(method == "L") %>%  # only run on Light Traps
-  distinct(trap_id, zone, trap_status) %>%
-  group_by(zone, trap_status) %>%
-  count()
-  
-  p = df %>% 
+    dplyr::filter(method == "L") %>% # only run on Light Traps
+    distinct(trap_id, zone, trap_status) %>%
+    group_by(zone, trap_status) %>%
+    count()
+
+  p = df %>%
     ggplot(aes(x = zone, y = n, fill = trap_status, color = trap_status)) +
     geom_col(alpha = 0.7) +
     theme_classic()
-  
+
   df_long = df %>%
-  pivot_wider(names_from = trap_status, 
-              values_from = n, 
-              values_fill = 0) %>%
-  ungroup %>%
-  mutate(trap_L = rowSums(select(., where(is.numeric))))
-  
-  
-  output = list("p" = p,
-                df = df_long)
-  
+    pivot_wider(names_from = trap_status, values_from = n, values_fill = 0) %>%
+    ungroup %>%
+    mutate(trap_L = rowSums(select(., where(is.numeric))))
+
+  output = list("p" = p, df = df_long)
+
   return(output)
 }
